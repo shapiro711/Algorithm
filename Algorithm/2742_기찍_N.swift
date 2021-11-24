854e8616c00f9361e0571ff7d6440a8d715117d1
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-24 13:40:17 +0900

    feat: 기찍N 풀이

---
+++ Algorithm/2742_기찍_N.swift
@@ -0,0 +1,14 @@
+//
+//  2742_기찍_N.swift
+//  Algorithm
+//
+//  Created by Kim Do hyung on 2021/11/24.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+
+for i in stride(from: n, through: 1, by: -1) {
+    print(i)
+}
