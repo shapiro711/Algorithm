ece698baf5b7ed69487a3b2f94123c52e463f303
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-25 16:42:19 +0900

    feat: X보다 작은 수 풀이

--- Algorithm/10871_X보다_작은_수.swift
+++ Algorithm/10871_X보다_작은_수.swift
@@ -1,9 +1,11 @@
 //
-//  main.swift
+//  10871_X보다_작은_수.swift
 //  Algorithm
 //
-//  Created by Kim Do hyung on 2021/11/23.
+//  Created by Kim Do hyung on 2021/11/25.
 //
+
+import Foundation
 
 import Foundation
 
