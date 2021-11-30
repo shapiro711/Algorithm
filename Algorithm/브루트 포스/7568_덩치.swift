71df83d01e294f31e214523715b4e1a3c04b9072
shpairo711 <shapiro7@ajou.ac.kr>
2021-11-30 20:37:57 +0900

    feat: 정렬 문제들 풀이

---
+++ Algorithm/브루트 포스/7568_덩치.swift
@@ -0,0 +1,16 @@
+//
+//  7568_덩치.swift
+//  Algorithm
+//
+//  Created by Kim Do hyung on 2021/11/29.
+//
+
+import Foundation
+
+var numberOfPerson = Int(readLine()!)!
+
+
+for _ in 1...numberOfPerson {
+    let firstLine = readLine()!.split(separator: " ").map(){ Int(String($0))! }
+}
+
---
+++ Algorithm/정렬/2751_수_정렬하기-2.swift
@@ -0,0 +1,23 @@
+//
+//  2751_수_정렬하기-2.swift
+//  Algorithm
+//
+//  Created by Kim Do hyung on 2021/11/30.
+//
+
+import Foundation
+
+var n = Int(readLine()!)!
+var numbers: [Int] = []
+
+for _ in 1...n {
+    var number = Int(readLine()!)!
+    numbers.append(number)
+}
+
+func mergeSort(a: Int, b:Int) {
+    if a == b {
+        return
+    }
+    
+}
